68ef8173db616728dd83c070419230c140d159ae
Kevin Yee <kevinyee1993@gmail.com>
2018-06-22 11:34:56 -0700

    Start basic style for button wrapper

--- src/containers/button_wrapper.jsx
+++ src/containers/button_wrapper.jsx
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 export default class ButtonWrapper extends Component {
   render() {
     return(
-      <div>
-        Hello I am the wrapper!
+      <div className="button-wrapper">
       </div>
     );
   }
