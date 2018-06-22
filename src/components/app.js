ce51052b374950018f52de6b85bcf62a4ddf85f4
Kevin Yee <kevinyee1993@gmail.com>
2018-06-22 11:32:26 -0700

    Create button wrapper

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import ButtonWrapper from '../containers/button_wrapper';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <ButtonWrapper />
     );
   }
 }
---
+++ src/containers/button_wrapper.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ButtonWrapper extends Component {
+  render() {
+    return(
+      <div>
+        Hello I am the wrapper!
+      </div>
+    );
+  }
+}
