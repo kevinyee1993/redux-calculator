21350b9cac491af8400609ed9d1ae2e8c1a6a929
Kevin Yee <kevinyee1993@gmail.com>
2018-06-22 11:36:55 -0700

    Start button component

---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Button = (props) => {
+  return(
+    <div>
+      BUTTON
+    </div>
+  );
+};
+
+export default Button;
--- src/containers/button_wrapper.jsx
+++ src/containers/button_wrapper.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Button from '../components/button';
 
 export default class ButtonWrapper extends Component {
   render() {
     return(
       <div className="button-wrapper">
+        <Button />
+        <Button />
+        <Button />
       </div>
     );
   }
