0ba63b421dd9657b020e696bb0a564f7eafc4c42
Kevin Yee <kevinyee1993@gmail.com>
2018-06-28 17:00:35 -0700

    Progress on operation button component

--- src/components/operation_button.jsx
+++ src/components/operation_button.jsx
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 
 export default class OperationButton extends Component {
 
-  switcheroo(currentNumber, previousNumber) {
+  constructor(props) {
+    super(props);
 
+    // thinking about using this to keep track of the numbers
+    this.state = { currentNumber: "", previousNumber: "" };
   }
+
+  // will return the new current number
+  calculate(previousNumber, currentNumber, operation) {
+    // use switch statements here for different operations, no operation
+    // or one number is empty means just return currentNumber
+
+    let prevToInt = parseInt(previousNumber);
+    let currToInt = parseInt(currentNumber);
+
+    console.log(prevToInt);
+    console.log(currToInt);
+
+    if(prevToInt) {
+      return (prevToInt + currToInt).toString();
+    } else {
+      return currToInt.toString();
+    }
+  }
+
+  // need to somehow update prevNum to currNum after operation button is pressed
+
 
   render() {
     let toggleClass;
+    let updatedValue;
 
     if(this.props.operation === this.props.value) {
       toggleClass = 'selected-button';
@@ -15,9 +40,13 @@ export default class OperationButton extends Component {
       toggleClass = 'button';
     }
 
+    updatedValue = this.calculate(this.props.previouNumber,
+      this.props.currentNumber,
+      this.props.operation);
+
     return(
       <div className={ toggleClass }
-        onClick={ () => this.props.selectOperation(this.props.value, "5", "10") }>
+        onClick={ () => this.props.selectOperation(this.props.value, updatedValue, "10") }>
 
         { this.props.value }
       </div>
